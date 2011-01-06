2c4f7c372c0548f8043b9af7797f19677441c280
Xinyu Chen <xinyu.chen@freescale.com>
2011-01-06 16:50:57 +0800

    ENGR00137445-1 Update Android.mk to add LOCAL_MODULE_TAGS := eng
    Update Android.mk to add LOCAL_MODULE_TAGS := eng
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- firmware/vpu/Android.mk
+++ firmware/vpu/Android.mk
@@ -14,7 +14,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := $(vpu_fw_file)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(vpu_fw_target)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
